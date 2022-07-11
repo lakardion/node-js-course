4dccae0fc6b1bbe8051d4f74462088da5b56b9f5
lakardion <lakardion@protonmail.com>
2022-07-11 15:17:02 -0300

    feat(DynamicContent) module adding minor changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const adminRoutes = require("./routes/admin");
+const { adminRoutes } = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const path = require("path");
 const { rootDir } = require("./helpers/rootDir");
--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const { rootDir } = require("../helpers/rootDir");
+const { products } = require("./admin");
 
 router.get("/", (req, res, next) => {
+  console.log({ products });
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
