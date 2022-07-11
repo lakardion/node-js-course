a2ca0340fb822caf4fcaea97692e980e55a027dd
lakardion <lakardion@protonmail.com>
2022-07-11 16:17:03 -0300

    feat (templating engines) ✨ copypaste handlebars and ejs

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const { adminRoutes } = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const path = require("path");
-const { rootDir } = require("./helpers/rootDir");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-// compile dynamic content
-app.set("view engine", "pug");
-app.set("views", "views");
-app.use(bodyParser.urlencoded());
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/admin", adminRoutes);
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req, res) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "not-found.html"));
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
